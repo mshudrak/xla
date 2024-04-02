e0c44c0b9fa007688384655d2107d611662b2ed7
manfeibaigithub <manfei@google.com>
2024-04-02 21:39:55 +0000

    upstream

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -32,6 +32,8 @@ def while_loop(cond_fn, body_fn, *carried_inputs, additional_inputs=None):
   # TODO(@manfei): PyTorch require operands to be list/tuple, PyTorch/XLA _xla_while_loop only accept *operands, *operands would tuple items again: (a, '')
   # cond_fn&body_fn: callable
   # carried_inputs: (Tuple of possibly nested dict/list/tuple of tensors)
+  print("000 carried_inputs: ", carried_inputs)
+  print("000 additional_inputs: ", additional_inputs)
   if additional_inputs is None:
     additional_inputs = tuple()
   return _xla_while_loop(cond_fn, body_fn, *carried_inputs, additional_inputs)
@@ -42,7 +44,7 @@ def _xla_while_loop(cond_fn, body_fn, *carried_inputs, additional_inputs):
   print("additional_inputs: ", additional_inputs)
   # untuple carried_inputs from while_loop
   carried_inputs = carried_inputs[0]
-  additional_inputs = carried_inputs[1]
+  additional_inputs = carried_inputs[-1]
   # fake operands to split formal code
   operands = []
   for carried_input in carried_inputs:
