2b9a2e32a6f949a0cde081fe02ccbb033f487c8c
manfeibaigithub <manfei@google.com>
2024-04-02 17:56:25 +0000

    upstream

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -29,7 +29,7 @@ def fori_loop(lower, upper, body_fun, one_value, init_val):
 
 
 @while_loop_op.py_impl(DispatchKey.XLA)
-def while_loop(cond_fn, body_fn, *loop_carry):
+def while_loop(cond_fn, body_fn, loop_carry):
   # TODO(@manfei): PyTorch require operands to be list/tuple, PyTorch/XLA _xla_while_loop only accept *operands, *operands would tuple items again: (a, '')
   # init, limit_value = loop_carry
   # return _xla_while_loop(cond_fn, body_fn, init, limit_value)
