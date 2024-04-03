5adf6c1fe17285ddf502bff676574452f05a098c
manfeibaigithub <manfei@google.com>
2024-04-03 23:07:12 +0000

    test

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -101,6 +101,7 @@ def _xla_while_loop(cond_fn, body_fn, *carried_inputs, additional_inputs): # a, 
   # (s32[1], s32[1], s32[1], s32[10], s32[1], /*index=5*/s32[20], s32[20,10]) 
   # params = params[:-4]+
   params += [params.pop(-1)]
+  print("params: ", params)
   # add additional_inputs for init of xla::while
   # if type(additional_inputs) is tuple:
   #   additional_shapes = xb.tensor_shape(additional_inputs)
